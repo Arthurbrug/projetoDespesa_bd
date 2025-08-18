0091815a245c4300b4ac4ea4c9a65d1c32b7a3ff
Arthurbrug <161843847+Arthurbrug@users.noreply.github.com>
2025-08-18 20:52:32 -0300

    Add files via upload

---
+++ src/main/java/br/fiap/modelo/Despesa.java
@@ -0,0 +1,51 @@
+package br.fiap.modelo;
+
+import java.time.LocalDate;
+
+public class Despesa {
+    private Double valor;
+    private Long id;
+    private String descricao;
+    private Categoria categoria;
+    private LocalDate data;
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
